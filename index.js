076378d6b451c4d472c6b4da8c8c4104c942951b
ian <ianokipla@gmail.com>
2023-10-09 03:19:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+
 // Code your solution in this file!
+function distanceFromHqInBlocks(customerBlock){
+    return 50-customerBlock
+}
+
+function distanceFromHqInfeet(blocks){
+    return distanceFromHqInBlocks(blocks) *264
+}
+
+function distanceTravelledInFeet(start,destination){
+    return(destination-start)*264
+}
+
+function calculatesFarePrice(start,destination){
+    let (destination-start) *264
+}
+
+if (feet<= 400) {
+    return "The first 400 metres are Free"
+}else if (feet>400 && feet<= 2000){
+   return 'Your fare is ${feet*2} cents'  
+}else if (feet>2500) {
+    return " Cannot travel that far."
+}{
+  //pass  
+}
+ 
